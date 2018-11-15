3ed6b144c80ea635ac070463eb801af96e665031
Victor Fugiwara <victorfugiwara@gmail.com>
2018-11-15 14:00:06 -0200

    Fixed port to run

--- api/__init__.py
+++ api/__init__.py
@@ -32,9 +32,3 @@ def create_app(test_config=None):
     app.register_blueprint(api.blueprint)
 
     return app
-
-
-if __name__ == '__main__':
-    app = create_app()
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
